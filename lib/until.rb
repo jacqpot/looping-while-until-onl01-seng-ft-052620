87c7f5737382aa21064cfa9d6af7d2734248ab26
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-27 17:24:38 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -7,10 +7,8 @@ def using_until
   
   levitation_force = 6
   while levitation_force < 11
-      puts 'Wingardium Leviosa'
-  
+    puts 'Wingardium Leviosa'
     levitation_force += 1
-
   end  
 end
 
